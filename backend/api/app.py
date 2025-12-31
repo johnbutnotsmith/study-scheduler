e9f363b7e8103bc6acd815301c9d8c686b7fe664
johnbutnotsmith <johnbutnotsmith@gmail.com>
2025-12-31 02:57:30 +0100

    cors error handling

--- backend/api/app.py
+++ backend/api/app.py
@@ -8,7 +8,10 @@ app = FastAPI(title="Study Scheduler API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # allow Vercel + localhost
+    allow_origins=[
+        "https://study-scheduler-nine.vercel.app",
+        "http://localhost:3000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
